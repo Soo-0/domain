ab515ec1aa2b1118f87b1a173659613c70c6f2ad
soo_0 <yoonsoo.0e@gmail.com>
2022-11-23 10:26:00 +0900

    commit

---
+++ js/ui.js
@@ -0,0 +1,26 @@
+// var wrap;
+// var x = 0, y = 0;
+// var mx = 0, my = 0;
+
+window.onload = () => {
+    test();
+    datepicker.init();
+    
+}
+
+
+let test = () => {
+    console.log('test')
+}
+
+let datepicker = {
+    init : function(){
+        console.log('testsetset')
+    }
+}
+
+let datepicker2 = () =>{
+    function date(){
+        console.log('test')
+    }
+}
